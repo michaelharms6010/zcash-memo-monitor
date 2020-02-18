9fa8274dac63f1e2919368411ad7a1811616c565
Michael Harms <michael.harms6010@gmail.com>
2020-02-18 15:29:51 -0600

    built post objects

--- src/memoprocessor.py
+++ src/memoprocessor.py
@@ -1,14 +1,20 @@
 import sys, json
+import requests
+
+URL = "https://be.zeitpages.com/board"
 
 f = open("memooutput.txt", "w")
 txns = json.load(sys.stdin)
+txnlist = []
 
 for t in txns:
-    print(t["datetime"])
-    print(t["amount"])
-    print(t["memo"])
+    datetime = t["datetime"]
+    amount = t["amount"]
+    memo = t["memo"]
+    params = {"datetime": datetime, "amount": amount, "memo": memo}
+    txnlist.append(params)
 
-
+print(txnlist)
 # f.write(sys.stdin)
 
 print("Done!")
