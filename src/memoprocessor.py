9e12be8394bd7e1ea88e0f4da8268d149c44b396
Michael Harms <michael.harms6010@gmail.com>
2020-02-18 15:38:10 -0600

    made a post

--- src/memoprocessor.py
+++ src/memoprocessor.py
@@ -1,11 +1,13 @@
 import sys, json
 import requests
 
-URL = "https://be.zeitpages.com/board"
+# URL = "https://be.zeitpages.com/board"
+URL = "https://zeitpages-staging.herokuapp.com/board"
 
 f = open("memooutput.txt", "w")
 txns = json.load(sys.stdin)
 txnlist = []
+headers = {"content-type": "application/json"}
 
 for t in txns:
     datetime = t["datetime"]
@@ -14,7 +16,13 @@ for t in txns:
     params = {"datetime": datetime, "amount": amount, "memo": memo}
     txnlist.append(params)
 
-print(txnlist)
+# if txn is new:
+for post in txnlist:
+    r = requests.post(url = URL, data=json.dumps(post), headers=headers)
+    print(r)
+
+
+
 # f.write(sys.stdin)
 
 print("Done!")
