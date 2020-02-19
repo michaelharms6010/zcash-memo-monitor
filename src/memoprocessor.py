48cd2ce3efd7aae09b634ed5d962c8a37ae6a0ac
Michael Harms <michael.harms6010@gmail.com>
2020-02-18 19:07:37 -0600

    moves to local record keeping

--- src/memoprocessor.py
+++ src/memoprocessor.py
@@ -1,33 +1,27 @@
 import sys, json
 import requests
+import ast
 
 # URL = "https://be.zeitpages.com/board"
 URL = "https://zeitpages-staging.herokuapp.com/board"
 
-f = open("memooutput.txt", "w")
+oldtxns = open("memooutput.txt", "r")
+records = []
+for i in oldtxns:
+    records.append(ast.literal_eval(i))
+
+f = open("memooutput.txt", "a")
 txns = json.load(sys.stdin)
 txnlist = []
 headers = {"content-type": "application/json"}
-f.write("[")
-first = True
 for t in txns:
     datetime = t["datetime"]
     amount = t["amount"]
     memo = t["memo"]
     params = {"datetime": datetime, "amount": amount, "memo": memo}
     txnlist.append(params)
-    if first:
-        f.write(str(params))
-    else:
-        f.write("," + str(params))
 
-f.write("]")
-# if txn is new:
-
-g = requests.get(url = URL)
-postlist = g.json()
-times = [int(i["datetime"]) for i in postlist]
-print(times)
+times = [int(i["datetime"]) for i in records]
 
 for post in txnlist:
     if post["memo"] == None or post["amount"] < 100000:
@@ -37,9 +31,6 @@ for post in txnlist:
     else:
         r = requests.post(url = URL, data=json.dumps(post), headers=headers)
         print(r)
-
-
-
-# f.write(sys.stdin)
+        f.write(str(post)+ "\n")
 
 print("Done!")
