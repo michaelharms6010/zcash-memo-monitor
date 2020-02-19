ecd33d212544a4fd9d2b18a74c0e723329399c97
Michael Harms <michael.harms6010@gmail.com>
2020-02-18 20:42:31 -0600

    updates file syntax

--- src/memoprocessor.py
+++ src/memoprocessor.py
@@ -5,32 +5,32 @@ import ast
 # URL = "https://be.zeitpages.com/board"
 URL = "https://zeitpages-staging.herokuapp.com/board"
 
-oldtxns = open("memooutput.txt", "r")
-records = []
-for i in oldtxns:
-    records.append(ast.literal_eval(i))
+with open("memooutput.txt", "r") as oldtxns:
+    records = []
+    for i in oldtxns:
+        records.append(ast.literal_eval(i))
 
-f = open("memooutput.txt", "a")
-txns = json.load(sys.stdin)
-txnlist = []
-headers = {"content-type": "application/json"}
-for t in txns:
-    datetime = t["datetime"]
-    amount = t["amount"]
-    memo = t["memo"]
-    params = {"datetime": datetime, "amount": amount, "memo": memo}
-    txnlist.append(params)
+    with open("memooutput.txt", "a") as f:
+        txns = json.load(sys.stdin)
+        txnlist = []
+        headers = {"content-type": "application/json"}
+        for t in txns:
+            datetime = t["datetime"]
+            amount = t["amount"]
+            memo = t["memo"]
+            params = {"datetime": datetime, "amount": amount, "memo": memo}
+            txnlist.append(params)
 
-times = [int(i["datetime"]) for i in records]
+        times = [int(i["datetime"]) for i in records]
 
-for post in txnlist:
-    if post["memo"] == None or post["amount"] < 100000:
-        continue
-    elif post["datetime"] in times:
-        continue
-    else:
-        r = requests.post(url = URL, data=json.dumps(post), headers=headers)
-        print(r)
-        f.write(str(post)+ "\n")
+        for post in txnlist:
+            if post["memo"] == None or post["amount"] < 100000:
+                continue
+            elif post["datetime"] in times:
+                continue
+            else:
+                r = requests.post(url = URL, data=json.dumps(post), headers=headers)
+                print(r)
+                f.write(str(post)+ "\n")
 
-print("Done!")
+    print("Done!")
